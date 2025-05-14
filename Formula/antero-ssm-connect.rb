17fe530e87843e8e2fb8cac842186897343343ae
Ilker Ispir <ilker@ispir.dev>
2025-05-14 11:13:54 +1000

    fix: updated sha for packages

--- Formula/antero-ssm-connect.rb
+++ Formula/antero-ssm-connect.rb
@@ -5,10 +5,10 @@ class AnteroSsmConnect < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/antero-software/antero-ssm-connect/releases/download/v1.0.0/antero-ssm-connect-darwin-amd64.tar.gz"
-      sha256 "32e09db295edf7efb1c27dddabb3519c628dfea3ec94d63e5879cdf8833d46e5"
+      sha256 "8a6f8957377c85ee421a943c659f146fb6d609dd52af3e5aaf77ea5b16d72ce3"
     elsif Hardware::CPU.arm?
       url "https://github.com/antero-software/antero-ssm-connect/releases/download/v1.0.0/antero-ssm-connect-darwin-arm64.tar.gz"
-      sha256 "2570ed407bb6584cd0b61d7804336384d1e0153bd8c7cbadd7fc4a7f2cc61511"
+      sha256 "a402a16a95328fc67e720b42d4e04074ae7bb5e97848a15514b961b181bd0328"
     end
   end
 
